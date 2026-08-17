151df7246f835c87fc9017e01a919f069ab8f4e8
= <=>
2026-08-17 22:21:16 +0530

    python learning day13

--- conditionals.py
+++
@@ -1,9 +0,0 @@
-askingNumber = input('Please enter any number: ')
-userEnteredNumber = float(askingNumber)
-
-if userEnteredNumber > 0:
-    print(f"You entered number {userEnteredNumber} is Positive")
-elif userEnteredNumber < 0:
-    print(f"You entered number {userEnteredNumber} is Negative")
-else:
-    print(f"You entered number {userEnteredNumber} is Zero")
--- dataTypeAndInput.py
+++
@@ -1,9 +0,0 @@
-name = 'Santhosh'
-age = 28
-
-askingName = input('Please enter you name: ')
-userName = str(askingName)
-askingAge = input('Please enter you age: ')
-userAge = int(askingAge)
-
-print(f'Hi {userName}, next year you\'ll be {userAge + 1}')
