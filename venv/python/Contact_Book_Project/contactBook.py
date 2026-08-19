0ed908006ad5b48b9b2efabef3251040d8418bb4
= <=>
2026-08-19 22:31:46 +0530

    python learning day14

--- venv/python/Contact_Book_Project/contactBook.py
+++ venv/python/Contact_Book_Project/contactBook.py
@@ -11,60 +11,61 @@
 # Repeat from step 2, until Exit is chosen
 
 contacts = {}
-try:
-    userChoosenOption = int(input(f"Pick an option: \n 1 for Add \n 2 for Search \n 3 for Update \n 4 for Delete \n 5 for VIew All \n 6 for Exit \n"))
-    while True:
-        if userChoosenOption == 1: #Add
-            print("Choosed option is Add")
-            name = input(f'Enter the name: ')
-            if name in contacts:
-                print(f'Given name already exists')
+
+while True:
+    try:
+        userChoosenOption = int(input(f"Pick an option: \n 1 for Add \n 2 for Search \n 3 for Update \n 4 for Delete \n 5 for VIew All \n 6 for Exit \n"))
+    except Exception as e:
+        print(f'Error in user input {e}')
+        continue
+
+    if userChoosenOption == 1: #Add
+        print("Choosed option is Add")
+        name = input(f'Enter the name: ')
+        if name in contacts:
+            print(f'Given name already exists')
+        else:
+            contacts[name] = int(input(f'Enter the number: '))
+            print(f'Contact added successfully!')
+
+    elif userChoosenOption == 2: #Search
+        print("Choosed option is Search")
+        givenName = input(f'Enter the name to search: ')
+        count = 0
+        for name, number in contacts.items():
+            if givenName.lower() in name.lower():
+                print(f'{name}: {number}')
             else:
-                contacts[name] = int(input(f'Enter the number: '))
-                print(f'Contact added successfully!')
+                count += 1
+                if count == len(contacts):
+                    print(f'Entered name is not in the contact:(')
 
-        elif userChoosenOption == 2: #Search
-            print("Choosed option is Search")
-            givenName = input(f'Enter the name to search: ')
-            count = 0
-            for name, number in contacts.items():
-                if givenName.lower() in name.lower():
-                    print(f'{name}: {number}')
-                else:
-                    count += 1
-                    if count == len(contacts):
-                        print(f'Entered name is not in the contact:(')
+    elif userChoosenOption == 3: #Update
+        print("Choosed option is Update")
+        name = input(f'Enter the name to search: ')
+        if name in contacts:
+            contacts[name] = int(input("Update the number: "))
+            print(f'{name} updated successfully!')
+        else:
+            print("Entered name is not in the contact:(")
 
-        elif userChoosenOption == 3: #Update
-            print("Choosed option is Update")
-            name = input(f'Enter the name to search: ')
-            if name in contacts:
-                contacts[name] = int(input("Update the number: "))
-                print(f'{name} updated successfully!')
+    elif userChoosenOption == 4: #Delete
+        print("Choosed option is Delete")
+        givenName = input(f'Enter the name to search: ')
+        for name, number in contacts.items(): #This is the alternate method using loop
+            if givenName == name:
+                deletedKeyNum = contacts.pop(givenName, None)
+                print(f'Contact {name}: {deletedKeyNum} is deleted successfully!')
+                break
             else:
                 print("Entered name is not in the contact:(")
 
-        elif userChoosenOption == 4: #Delete
-            print("Choosed option is Delete")
-            givenName = input(f'Enter the name to search: ')
-            for name, number in contacts.items(): #This is the alternate method using loop
-                if givenName == name:
-                    deletedKeyNum = contacts.pop(givenName, None)
-                    print(f'Contact {name}: {deletedKeyNum} is deleted successfully!')
-                    break
-                else:
-                    print("Entered name is not in the contact:(")
+    elif userChoosenOption == 5: #View All
+        print("Choosed option is View All")
+        print("All contacts: ")
+        for name, number in contacts.items():
+            print(f'{name}: {number}')
 
-        elif userChoosenOption == 5: #View All
-            print("Choosed option is View All")
-            print("All contacts: ")
-            for name, number in contacts.items():
-                print(f'{name}: {number}')
-
-        elif userChoosenOption == 6:
-            print("Thank you :)")
-            break
-        userChoosenOption = int(input(f"Pick an option: \n 1 for Add \n 2 for Search \n 3 for Update \n 4 for Delete \n 5 for VIew All \n 6 for Exit \n"))
-
-except Exception as e:
-    print(f'Error in user input {e}')
+    elif userChoosenOption == 6:
+        print("Thank you :)")
+        break
